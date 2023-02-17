3de72c0a4b9dcf4a2fc03a25bce3e91efba27a0b
waiwoot suriyamanee <waiwootsuriyamanee.com>
2023-02-17 09:53:34 +0700

    Add Version File

--- AMR_WiFi/AMR_Wifi.ino
+++ AMR_WiFi/AMR_Wifi.ino
@@ -2,6 +2,9 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>    //https://github.com/esp8266/Arduino
+
+
+
 #include <ESP8266HTTPClient.h>
 //#include <DNSServer.h>
 #include <ESP8266WebServer.h>
@@ -67,6 +70,9 @@ void WiFiEvent(WiFiEvent_t event) {
     }
 }
 */
+
+
+
 void setup()
 {
 
@@ -128,41 +134,7 @@ wifiManager.autoConnect("AutoConnectAP");
   Serial.println("Station: "+String(Site));
   Serial.print("ESP.getChip:");
   Serial.println(ESP.getChipId());
-/*//////////////////// การ upload firmware ผ่านทาง WiFi \\\\\\\\\\\\*/
-/*     ArduinoOTA.onStart([]() {
-            String type;
-                 if (ArduinoOTA.getCommand() == U_FLASH) {
-                     type = "sketch";
-                        } else { // U_FS
-                                type = "filesystem";
-                                }
-                          // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-                                Serial.println("Start updating " + type);
-                               });
-     ArduinoOTA.onEnd([]() {
-                Serial.println("\nEnd");
-                                 });
-     ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-                Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-                                 });
-    ArduinoOTA.onError([](ota_error_t error) {
-                                         Serial.printf("Error[%u]: ", error);
-                                         if (error == OTA_AUTH_ERROR) {
-                                          Serial.println("Auth Failed");
-                                       } else if (error == OTA_BEGIN_ERROR) {
-                                           Serial.println("Begin Failed");
-                                       } else if (error == OTA_CONNECT_ERROR) {
-                                           Serial.println("Connect Failed");
-                                       } else if (error == OTA_RECEIVE_ERROR) {
-                                          Serial.println("Receive Failed");
-                                      } else if (error == OTA_END_ERROR) {
-                                           Serial.println("End Failed");
-                                       }
-                                           });
-          ArduinoOTA.begin();
 
-          */
-                          /*//////////////////// การ upload firmware ผ่านทาง WiFi \\\\\\\\\\\\*/
 
    upintheair();
   Scheduler.start(&send_task);
